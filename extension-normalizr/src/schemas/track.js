dea1296fc58d7ffe63a7cd68503d19865362e4f5
Robin Wieruch <wrobin@gmx.net>
2016-07-09 14:00:27 +0200

    feat(normalizr): normalize user

--- extension-normalizr/src/schemas/track.js
+++ extension-normalizr/src/schemas/track.js
@@ -1,5 +1,10 @@
 import { Schema } from 'normalizr';
+import userSchema from './user';
 
 const trackSchema = new Schema('tracks');
+
+trackSchema.define({
+  user: userSchema
+});
 
 export default trackSchema;
---
+++ extension-normalizr/src/schemas/user.js
@@ -0,0 +1,5 @@
+import { Schema } from 'normalizr';
+
+const userSchema = new Schema('users');
+
+export default userSchema;
