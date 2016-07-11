5037d4169d3be85818dc538072105f4ffc162227
Robin Wieruch <wrobin@gmx.net>
2016-07-11 21:10:26 +0200

    feat(css-modules): init

---
+++ extension-css-modules/src/index.js
@@ -0,0 +1,27 @@
+import SC from 'soundcloud';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import configureStore from './stores/configureStore';
+import App from './components/App';
+import Callback from './components/Callback';
+import Stream from './components/Stream';
+
+const store = configureStore();
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Stream} />
+        <Route path="/" component={Stream} />
+        <Route path="/callback" component={Callback} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
