faf99c9447a258ad0572f99a8f41f0a8f11bc4ec
Robin Wieruch <wrobin@gmx.net>
2016-07-16 18:49:25 +0200

    feat(userStore): userStore + auth

--- extension-mobx/src/actions/auth.js
+++ extension-mobx/src/actions/auth.js
@@ -1,6 +1,7 @@
 import { CLIENT_ID, REDIRECT_URI } from '../constants/auth';
 import * as actionTypes from '../constants/actionTypes';
 import { setTracks } from '../actions/track';
+import userStore from '../stores/userStore';
 
 function setMe(user) {
   return {
@@ -10,18 +11,19 @@ function setMe(user) {
 }
 
 export function auth() {
-    return function (dispatch) {
-        SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
+  SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
 
-        SC.connect().then((session) => {
-          fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
-            .then((response) => response.json())
-            .then((me) => {
-              dispatch(setMe(me));
-              dispatch(fetchStream(me, session));
-            });
-        });
-    };
+  SC.connect().then((session) => {
+    fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
+      .then((response) => response.json())
+      .then((me) => {
+        userStore.me = me;
+        console.log(userStore);
+        console.log(userStore.me);
+        // dispatch(setMe(me));
+        // dispatch(fetchStream(me, session));
+      });
+  });
 };
 
 function fetchStream(me, session) {
---
+++ extension-mobx/src/stores/userStore.js
@@ -0,0 +1,15 @@
+import { observable, computed } from 'mobx';
+
+class UserStore {
+
+  @observable me;
+
+  constructor() {
+    this.me = null;
+  }
+
+}
+
+const userStore = new UserStore();
+
+export default userStore;
