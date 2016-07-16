c955eb361845336820823702bbb6f580d1545fd3
Robin Wieruch <wrobin@gmx.net>
2016-07-16 19:04:21 +0200

    feat(trackStore): add activeTrack in store

--- extension-mobx/src/stores/trackStore.js
+++ extension-mobx/src/stores/trackStore.js
@@ -3,9 +3,11 @@ import { observable, computed } from 'mobx';
 class TrackStore {
 
   @observable tracks;
+  @observable activeTrack;
 
   constructor() {
     this.tracks = null;
+    this.activeTrack = null;
   }
 
 }
