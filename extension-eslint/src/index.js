220bef0dbf52cf28054d58898a6eac6f18ea2d05
Robin Wieruch <wrobin@gmx.net>
2016-08-30 19:13:33 +0200

    feat(extension-eslint): refactor fetch and SC init

--- extension-eslint/src/index.js
+++ extension-eslint/src/index.js
@@ -1,6 +1,4 @@
-/*eslint-disable no-unused-vars*/
 import SC from 'soundcloud';
-/*eslint-enable no-unused-vars*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
@@ -11,6 +9,9 @@ import * as actions from './actions';
 import App from './components/App';
 import Callback from './components/Callback';
 import Stream from './components/Stream';
+import { CLIENT_ID, REDIRECT_URI } from './constants/auth';
+
+SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
 
 const store = configureStore();
 
